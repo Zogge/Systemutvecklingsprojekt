f75acf7c6509e34ae8ad66bc70292e8406043cf1
Zogge <oskar@rosenback.eu>
2022-04-19 09:35:47 +0200

    shf

---
+++ Assets/Scripts/KeyBindings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class KeyBindings
+{
+    // Start is called before the first frame update
+    static void Test()
+    {
+        
+    }
+}
